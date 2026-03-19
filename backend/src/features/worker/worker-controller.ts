29472059e222f00950672bca1598a3903fab84b8
Leczok12 <kamilleczo@op.pl>
2026-03-19 10:49:18 +0100

    worker endpoint

--- backend/src/features/worker/worker-controller.ts
+++ backend/src/features/worker/worker-controller.ts
@@ -3,8 +3,10 @@ import { ApiResponse } from '@shared/api-response';
 import { database } from 'src/config/database';
 import { ApiError } from 'src/types/api-error';
 import { ApiWorkerResponse } from '@shared/api-worker';
+import { roleCheck } from 'src/utils/role-check';
 
 export const getAllWorkers = async (req: Request, res: Response) => {
+    roleCheck(req, 'VIEWER');
     const pageNumber = parseInt((req.query.pageNumber as string | undefined) ?? '1');
     const pageSize = parseInt((req.query.pageSize as string | undefined) ?? '9999');
 
@@ -40,6 +42,7 @@ export const getAllWorkers = async (req: Request, res: Response) => {
 };
 
 export const findWorkers = async (req: Request, res: Response) => {
+    roleCheck(req, 'VIEWER');
     const keyword = req.query.keyword as string | undefined;
     const pageNumber = parseInt(req.query.pageNumber as string) || 1;
     const pageSize = parseInt(req.query.pageSize as string) || 9999;
@@ -85,6 +88,46 @@ export const findWorkers = async (req: Request, res: Response) => {
             }),
             pageSize: pageSize,
         },
+    };
+
+    res.status(200).json(response);
+};
+
+export const getWorkerById = async (req: Request, res: Response) => {
+    roleCheck(req, 'WORKER');
+    const workerId = req.params.id;
+
+    const worker = await database.prisma.worker.findUnique({ where: { id: workerId.toString() } });
+
+    if (!worker) {
+        throw new ApiError(404, 'NOT_FOUND');
+    }
+
+    const response: ApiResponse<ApiWorkerResponse> = {
+        status: 'SUCCESS',
+        data: worker,
+    };
+
+    res.status(200).json(response);
+};
+
+export const postWorkerById = async (req: Request, res: Response) => {
+    roleCheck(req, 'SYSTEM_ADMIN');
+    const workerId = req.params.id;
+    const rawSync = req.body?.sync;
+
+    if (rawSync === undefined || typeof rawSync !== 'boolean') {
+        throw new ApiError(400, 'INVALID_ARGS');
+    }
+
+    const worker = await database.prisma.worker.update({
+        where: { id: workerId.toString() },
+        data: { sync: rawSync },
+    });
+
+    const response: ApiResponse<ApiWorkerResponse> = {
+        status: 'SUCCESS',
+        data: worker,
     };
 
     res.status(200).json(response);
--- backend/src/features/worker/worker-router.ts
+++ backend/src/features/worker/worker-router.ts
@@ -1,17 +1,19 @@
 import express from 'express';
 import { ApiError } from 'src/types/api-error';
-import { getAllWorkers, findWorkers } from './worker-controller';
+import { getAllWorkers, findWorkers, getWorkerById, postWorkerById } from './worker-controller';
 import { Request, Response } from 'express';
 import { roleCheck } from 'src/utils/role-check';
 
 const router = express.Router();
 
-router.use((req: Request, res: Response, next: Function) => {
-    roleCheck(req, 'VIEWER');
-    next();
-});
+// router.use((req: Request, res: Response, next: Function) => {
+//     roleCheck(req, 'VIEWER');
+//     next();
+// });
 
 router.get('/all', getAllWorkers);
 router.get('/find', findWorkers);
+router.get('/:id', getWorkerById);
+router.post('/:id', postWorkerById);
 
 export { router as workerRouter };

f25452170cc820da03f9076f920869914629089b
Leczok12 <kamilleczo@op.pl>
2026-04-09 22:37:23 +0200

    apie admin device endpoint working

--- backend/src/features/admin/device/device-controller.ts
+++ backend/src/features/admin/device/device-controller.ts
@@ -1,15 +1,22 @@
 import { Request, Response } from 'express';
 import { ApiResponse } from '@shared/api-response';
 import { database } from 'src/config/database';
-import { ApiAdminUpdateDeviceRequest } from '@shared/api-admin-device';
+import { ApiAdminGetDeviceResponse, ApiAdminUpdateDeviceData } from '@shared/api-admin-device';
 import { ApiError } from 'src/types/api-error';
 import { $Enums, DeviceType } from '@prisma/client';
 import z from 'zod';
 
+/* Get all devices */
+
 export const getAllDevices = async (req: Request, res: Response) => {
-    const response: ApiResponse<ApiAdminUpdateDeviceRequest[]> = {
+    const response: ApiResponse<ApiAdminGetDeviceResponse[]> = {
         status: 'SUCCESS',
-        data: (await database.prisma.device.findMany()) ?? [],
+        data:
+            (await database.prisma.device.findMany()).map((device) => ({
+                id: device.id,
+                type: device.type,
+                name: device.name,
+            })) ?? [],
     };
 
     res.status(200).json(response);
@@ -17,7 +24,7 @@ export const getAllDevices = async (req: Request, res: Response) => {
 
 /* Update device */
 
-const updateDevicesSchema: z.Schema<ApiAdminUpdateDeviceRequest[]> = z.array(
+const updateDevicesSchema: z.Schema<ApiAdminUpdateDeviceData[]> = z.array(
     z.object({
         id: z.string(),
         type: z.enum($Enums.DeviceType),
@@ -26,14 +33,14 @@ const updateDevicesSchema: z.Schema<ApiAdminUpdateDeviceRequest[]> = z.array(
 
 export const updateDevices = async (req: Request, res: Response) => {
     console.log('Updating devices for worker', req.body);
-    const data = updateDevicesSchema.safeParse(req.body.data);
+    const data = updateDevicesSchema.safeParse(JSON.parse(req.body.data));
 
     if (data.success === false)
         throw new ApiError(400, 'INVALID_ARGS', data.error.issues.map((issue) => issue.message).join(', '));
 
-    database.prisma.$transaction(async (prisma) => {
+    await database.prisma.$transaction(async (prisma) => {
         for (const device of data.data) {
-            const existingDevice = prisma.device.findUnique({
+            const existingDevice = await prisma.device.findUnique({
                 where: {
                     id: device.id,
                 },

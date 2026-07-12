5e608a827458a3728ef5fe79e24f1db0438da539
Leczok12 <kamilleczo@op.pl>
2026-07-12 16:18:17 +0200

    root layout check user

--- frontend/src/layouts/root-layout.tsx
+++ frontend/src/layouts/root-layout.tsx
@@ -1,6 +1,33 @@
-import { Outlet } from 'react-router';
+import { getApiAuthUser } from '@src/api/api-auth';
+import { useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router';
 
 const RootLayout = () => {
+    const navigator = useNavigate();
+    const { data, isLoading, error } = useQuery({
+        queryKey: ['auth', 'user'],
+        queryFn: getApiAuthUser,
+        retry: false,
+        staleTime: 0, // 5 minutes
+        gcTime: 0, // 10 minutes
+    });
+
+    console.log('AdminLayout data:', data, error);
+
+    useEffect(() => {
+        if (data?.status === 'UNAUTHORIZED') {
+            navigator('/auth/login');
+        }
+        if (data?.status === 'SUCCESS' && data.data?.role === 'WORKER') {
+            navigator(`/worker/${data.data.workerId}`);
+        }
+    }, [data, navigator]);
+
+    if (isLoading || !data) {
+        return null;
+    }
+
     return (
         <div>
             <header>Header</header>
